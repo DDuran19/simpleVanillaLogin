d94c204b191b4058608e0f9070130bb7660807ba
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-07-30 06:45:20 +0800

    Add custom scripts for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 
-// const BASE = "/simpleVanillaLogin/";
-const BASE = "/";
+const BASE = "/simpleVanillaLogin/";
 export default defineConfig({
     base: process.env.NODE_ENV === "production" ? BASE : "/",
-    // base: BASE,
     build: {
         outDir: "./dist",
         emptyOutDir: true,
